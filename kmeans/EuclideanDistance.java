235893f3ca072fa27ec98c05f13851451bef0559
allevia <37606384+allevia@users.noreply.github.com>
2018-11-09 19:08:20 +0800

    kmeans

---
+++ kmeans/EuclideanDistance.java
@@ -0,0 +1,20 @@
+package kmeans;
+
+import java.util.List;
+
+public class EuclideanDistance<T extends Number> {
+	public double getDistance(List<T> a, List<T> b) throws Exception {
+		// TODO Auto-generated method stub
+		if(a.size() != b.size())
+			throw new Exception("size not compatible!");
+		else{
+			double sum = 0.0;
+			for(int i = 0;i < a.size();i++){
+				sum += Math.pow(a.get(i).doubleValue() - b.get(i).doubleValue(), 2);
+			}
+			sum = Math.sqrt(sum);
+			return sum;
+		}
+	}
+
+}
